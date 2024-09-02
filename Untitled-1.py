307bfbed34775b31565960e7733f200ccc71977e
Steffany Paiva <76757826+Steffany7406@users.noreply.github.com>
2024-09-02 20:39:11 -0300

    Add files via upload

---
+++ Untitled-1.py
@@ -0,0 +1,13 @@
+nome = "Steffany"
+idade = "24"
+gosto_de = "programação"
+
+
+print(f"Meu nome é {nome}.")
+print(f"Minha idade é {idade}")
+print(f"Eu gosto de {gosto_de}")
+print(f"Steffany gosta de {gosto_de}")
+
+idade = "36"
+
+print(f"{nome} tem {idade}")
